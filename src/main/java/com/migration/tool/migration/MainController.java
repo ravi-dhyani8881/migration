749686289370cd3f82f774be0b3b01a1f3e5822d
ravi <rdhyani@sapient.com>
2018-04-29 22:21:07 +0530

    Added Spring boot + html project

---
+++ src/main/java/com/migration/tool/migration/MainController.java
@@ -0,0 +1,28 @@
+package com.migration.tool.migration;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+
+	
+	@RequestMapping(value="/" , method= RequestMethod.GET)
+	public ModelAndView home() {
+						
+		return new ModelAndView("home");
+	}
+	
+	
+	@RequestMapping(value="/Schdular" , method= RequestMethod.GET )
+	public ModelAndView addSchdular(@RequestParam("sname") String username,@RequestParam("selected_date") String username2,@RequestParam("selected_date") String format) {
+						
+		System.out.println("Added Schdular"+username+"----->username2"+username2+"------->format"+format);
+		
+		return new ModelAndView("addSchdular").addObject("message", username);
+	}
+	
+}
+
+
---
+++ src/main/java/com/migration/tool/migration/MigrationApplication.java
@@ -0,0 +1,19 @@
+package com.migration.tool.migration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class MigrationApplication extends SpringBootServletInitializer  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigrationApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MigrationApplication.class);
+	}
+}
