c48063d01ec4bea0fcabeccc56ea6a039f2cc893
ravi <rdhyani@sapient.com>
2018-06-12 17:09:43 +0530

    Updated

--- src/main/angular-front/src/app/models/user.ts
+++ src/main/angular-front/src/app/models/user.ts
@@ -1,7 +1,11 @@
 
 export class User {
    constructor(
+      public id: Number,
+      public userName: string,
+      public password: string,
       public name: string,
-      public lastName: string
+      public lastName: string,
+      public email: string
    ) {}
 }
